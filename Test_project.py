4e4ed311a09ae2191692007c317a5d4bb391294b
= <=>
2024-04-18 21:29:08 +0200

    Stworzenie programu do znajdowania liczby

--- Test_project.py
+++ Test_project.py
@@ -2,5 +2,20 @@ import random
 
 
 print('Program do zgadywania liczby z zakresu od 1 do 10 000')
-liczba = input('Podaj liczbę: ')
+print('...')
+
+number = random.randint(1, 10000)
+
+while True:
+    liczba = int(input('Podaj liczbę: '))
+    if liczba > number:
+        print('Za dużo!')
+    elif liczba < number:
+        print('Za mało!')
+    elif liczba > 10000:
+        print('Liczba poza zakresem!')
+    elif liczba == number:
+        print('Gratulacje, odnalazłes liczbę!')
+        input()
+        break
 
